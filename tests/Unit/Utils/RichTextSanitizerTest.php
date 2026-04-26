1b9df34b4c17b84845fd7e834d13acb877638629
Sebastian Javier Maidana <seba_maidana_3@hotmail.com>
2026-04-26 17:23:50 -0300

    fix: aumento de cobertura en los test

--- tests/Unit/Utils/RichTextSanitizerTest.php
+++ tests/Unit/Utils/RichTextSanitizerTest.php
@@ -40,4 +40,59 @@ class RichTextSanitizerTest extends TestCase
         $this->assertSame('', RichTextSanitizer::sanitize(null));
         $this->assertSame('', RichTextSanitizer::sanitize('   '));
     }
+
+    public function testSanitizeDesenvuelveTagNoPermitidoYConservaContenidoInterno(): void
+    {
+        $html = '<section><p>Texto <strong>valido</strong></p></section>';
+
+        $result = RichTextSanitizer::sanitize($html);
+
+        $this->assertStringNotContainsString('<section', $result);
+        $this->assertStringContainsString('<p>Texto <strong>valido</strong></p>', $result);
+    }
+
+    public function testSanitizeAgregaRelCuandoAnchorTieneTargetBlank(): void
+    {
+        $html = '<a href="https://example.com" target="_blank">sitio</a>';
+
+        $result = RichTextSanitizer::sanitize($html);
+
+        $this->assertStringContainsString('href="https://example.com"', $result);
+        $this->assertStringContainsString('target="_blank"', $result);
+        $this->assertStringContainsString('rel="noopener noreferrer"', $result);
+    }
+
+    public function testSanitizeEliminaStyleConExpressionYCaracteresInvalidos(): void
+    {
+        $html = '<p style="text-align: center; color: expression(alert(1)); background-color: rgb(10, 10, 10); color: red@">x</p>';
+
+        $result = RichTextSanitizer::sanitize($html);
+
+        $this->assertStringContainsString('text-align: center', $result);
+        $this->assertStringContainsString('background-color: rgb(10, 10, 10)', $result);
+        $this->assertStringNotContainsString('expression', $result);
+        $this->assertStringNotContainsString('red@', $result);
+    }
+
+    public function testSanitizeClassPermiteSoloPrefijosEsperados(): void
+    {
+        $html = '<p class="ql-align-center text-red foo bar">hola</p>';
+
+        $result = RichTextSanitizer::sanitize($html);
+
+        $this->assertStringContainsString('class="ql-align-center text-red"', $result);
+        $this->assertStringNotContainsString('foo', $result);
+        $this->assertStringNotContainsString('bar', $result);
+    }
+
+    public function testSanitizeHrefPermiteRelativoYBloqueaDataUri(): void
+    {
+        $html = '<a href="/interna">ok</a><a href="data:text/html;base64,AA==">mal</a>';
+
+        $result = RichTextSanitizer::sanitize($html);
+
+        $this->assertStringContainsString('<a href="/interna">ok</a>', $result);
+        $this->assertStringContainsString('<a>mal</a>', $result);
+        $this->assertStringNotContainsString('data:text/html', $result);
+    }
 }
